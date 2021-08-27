e8e2534dba3dad80f088e7e0bdd5172462c5ce56
GEOVANY OQUENDO <giovanyom12@gmail.com>
2021-08-26 22:25:34 -0500

    Complete DB entities

--- MyVet.Web/Data/Entities/Owner.cs
+++ MyVet.Web/Data/Entities/Owner.cs
@@ -10,36 +10,36 @@ namespace MyVet.Web.Data.Entities
     {
         public int Id { get; set; }
 
-        [Required]
-        [MaxLength(30)]
+        [Display(Name = "Document")]
+        [MaxLength(30, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
         public string Document { get; set; }
 
-        [Required]
-        [MaxLength(50)]
         [Display(Name = "First Name")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
         public string FirstName { get; set; }
 
-        [Required]
-        [MaxLength(50)]
         [Display(Name = "Last Name")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
         public string LastName { get; set; }
 
-        [MaxLength(20)]
         [Display(Name = "Fixed Phone")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         public string FixedPhone { get; set; }
 
-        [Required]
-        [MaxLength(20)]
         [Display(Name = "Cell Phone")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         public string CellPhone { get; set; }
 
-        [MaxLength(100)]
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         public string Address { get; set; }
 
         [Display(Name = "Owner")]
-        public String FullName => $"{FirstName} {LastName}";
+        public string FullName => $"{FirstName} {LastName}";
 
-        [Display(Name = "Owner With Document")]
-        public String FullNameWithDocument => $"{FirstName} {LastName} {Document}";
+        [Display(Name = "Owner")]
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
     }
 }
